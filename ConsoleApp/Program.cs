573dda41a4ef073d52c959e0c30eafb7ebf542e6
larchenkoroman <larchenkoroman@gmail.com>
2025-01-04 00:35:50 +0200

    first commit

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -6,7 +6,7 @@ string cnn = "Data Source=(LocalDB)\\MSSQLLocalDB;Initial Catalog=TestDB;Integra
 Repository repo = new(cnn);
 repo.ExecuteNonQuery("update g set Amount = 555 from dbo.Goods as g where g.ID = @ID", new { ID = 2 });
 
-List<Product> list = repo.ExequteQuery<Product>("SELECT * FROM dbo.Goods WHERE ID = @ID", new { ID = 2 });
+List<Product> list = repo.ExequteQuery<Product>("SELECT ID,Name,Amount FROM dbo.Goods WHERE ID = @ID", new { ID = 2 });
 
 
 
